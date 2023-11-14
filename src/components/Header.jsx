6fc2105b60977ca6a6f111e208380c6deef07d45
leeSeungHoon <leesh6137@naver.com>
2023-11-14 11:41:55 +0900

    231114_11:41_헤더완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,27 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className="bg-white shadow">
-            <nav className="container mx-auto px-6 py-3">
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center">
-                        <div className="text-xl font-semibold text-gray-700">
-                            <a href="/" className="text-gray-800">uni Intern</a>
-                        </div>
-                      
-                        <div class="hidden md:flex items-center space-x-1 ml-10">
-                            <a href="/" className="py-2 px-2 text-gray-700 hover:text-gray-800">홈</a>
-                            <a href="/" className="py-2 px-2 text-gray-700 hover:text-gray-800">CCTV</a>
-                            <a href="/" className="py-2 px-2 text-gray-700 hover:text-gray-800">발생현황</a>
-                            <a href="/" className="py-2 px-2 text-gray-700 hover:text-gray-800">구조도</a>
-                        </div>
+        <header>
+            <nav className="mx-auto px-6 py-3">
+                <div className=" grid grid-cols-12 flex justify-between items-center p-3">
+
+
+                    {/* 로고 */}
+                    <div className="col-span-2">
+                        <p className="text-xl font-semibold text-gray-800">uni Intern</p>
                     </div>
-                 
-                 
-                    <div className="md:hidden flex items-center">
-                        <button className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
-                            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                              
-                                <path d="M4 5h16M4 11h16M4 17h16" />
-                            </svg>
-                        </button>
+
+                    {/* 버튼 */}
+                    <div class="col-span-10 items-center space-x-1 ml-10">
+                        <a href="/" className="py-2 px-2 text-gray-400 hover:text-gray-800">홈</a>
+                        <a href="/" className="py-2 px-2 text-gray-400 hover:text-gray-800">CCTV</a>
+                        <a href="/" className="py-2 px-2 text-gray-400 hover:text-gray-800">발생현황</a>
+                        <a href="/" className="py-2 px-2 text-gray-400 hover:text-gray-800">구조도</a>
                     </div>
+
                 </div>
+                {/* 밑줄 */}
+                <div class="mt-3 w-full border-t-4 border-gray-500" />
             </nav>
         </header>
 
