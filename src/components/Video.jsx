98c10845084f6b4e3a0afbf9dbfa6030befd5d10
leeSeungHoon <leesh6137@naver.com>
2023-11-14 15:19:01 +0900

    231114_1518_이승훈_비디오 크기 조절

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div className='border border-gray-400 mt-6 flex justify-center items-center'>
+    <div className="border border-gray-400 mt-6 flex justify-center">
       <img src="http://localhost:5000/" alt="Streaming from Flask Server" />
     </div>
+
   )
 }
 
