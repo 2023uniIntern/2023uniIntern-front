0e5b0c7d2e70fed8baf4bc13742f041e6ad4c310
leeSeungHoon <leesh6137@naver.com>
2023-11-15 17:01:03 +0900

    231115_17:00_이승훈_비디오 컴포넌트 레터박스 추가 로딩 추가

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
+// 외부API(로딩)
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Video = () => {
   const [error, setError] = useState(null);
   const [stream, setStream] = useState(0); // 스트림 상태 추가
-
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태 추가
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetch("http://localhost:5000/")
         .then(response => {
           if (!response.ok) {
-            // console.log("실패");
             throw new Error('스트리밍을 통신할 수 없습니다.');
           }
         })
         .catch(error => {
-          // console.log("실패");
           setError('스트리밍을 통신할 수 없습니다.');
+        })
+        .finally(() => {
+          setIsLoading(false); // 로딩 상태 업데이트
         });
     }, 10000); // 10초마다 체크
 
@@ -25,22 +30,33 @@ const Video = () => {
 
   const reconnectStream = () => {
     setError(null);
+    setIsLoading(true); // 재시도 시 로딩 상태 업데이트
     setStream(prevKey => prevKey + 1); // 스트림 키를 증가시켜 이미지를 재로드하게 함
   };
 
   return (
     <div className="bg-black rounded-lg shadow-md flex justify-center items-center overflow-hidden cursor-default">
-      {error ? (
-        <>
-          <p>{error}</p>
-          <button onClick={reconnectStream}>재연결하기</button>
-        </>
+      {isLoading ? (
+        <div className="w-full text-white text-center p-3 flex items-center justify-center" style={{ height: '350px' }}>
+          <FontAwesomeIcon icon={faSpinner} spinPulse size="2xl" />
+        </div>
+      ) : error ? (
+        <div className="w-full text-white text-center p-3 flex flex-col items-center justify-center" style={{ height: '350px' }}>
+          <p className="block">{error}</p>
+          <p className='block mt-5 hover:bg-white hover:text-black rounded-full border border-white p-3 cursor-pointer'
+            onClick={reconnectStream}>
+            연결 시도하기
+          </p>
+        </div>
       ) : (
         <img
           key={stream} // 스트림 상태를 key로 사용
           src="http://localhost:5000/"
           alt="Streaming from Flask Server"
-          onError={() => setError('스트리밍을 통신할 수 없습니다.')}
+          onError={() => {
+            setError('스트리밍을 통신할 수 없습니다.');
+            setIsLoading(false); // 에러 발생 시 로딩 상태 업데이트
+          }}
         />
       )}
     </div>
