343ae1d903b313fbab02f357d4ff543db8583c77
leeSeungHoon <leesh6137@naver.com>
2023-11-15 11:26:31 +0900

    231115_11:26_이승훈_비디오 재연결 시스템 구축완료

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,29 +2,42 @@ import React, { useState, useEffect } from 'react';
 
 const Video = () => {
   const [error, setError] = useState(null);
+  const [stream, setStream] = useState(0); // 스트림 상태 추가
+
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetch("http://localhost:5000/")
         .then(response => {
           if (!response.ok) {
+            // console.log("실패");
             throw new Error('스트리밍을 통신할 수 없습니다.');
           }
         })
         .catch(error => {
-          setError("스트리밍을 통신 할 수 없습니다.");
+          // console.log("실패");
+          setError('스트리밍을 통신할 수 없습니다.');
         });
-    }, 5000); // 5초마다 체크
+    }, 10000); // 10초마다 체크
 
     return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
   }, []);
 
+  const reconnectStream = () => {
+    setError(null);
+    setStream(prevKey => prevKey + 1); // 스트림 키를 증가시켜 이미지를 재로드하게 함
+  };
+
   return (
     <div>
       {error ? (
-        <p>{error}</p>
+        <>
+          <p>{error}</p>
+          <button onClick={reconnectStream}>재연결하기</button>
+        </>
       ) : (
         <img
+          key={stream} // 스트림 상태를 key로 사용
           src="http://localhost:5000/"
           alt="Streaming from Flask Server"
           onError={() => setError('스트리밍을 통신할 수 없습니다.')}
